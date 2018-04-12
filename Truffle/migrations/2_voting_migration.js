7358bd574bcf22e7f3c041299f75e183b88d0089
habichta <habichta@student.ethz.ch>
2018-04-13 01:36:46 +0200

    temp

--- Truffle/migrations/2_voting_migration.js
+++ Truffle/migrations/2_voting_migration.js
@@ -3,6 +3,6 @@ var Secp256k1 = artifacts.require("./Secp256k1.sol")
 var AnonymousVoting = artifacts.require("./AnonymousVoting.sol")
 
 module.exports = (deployer) => {
-deployer.deploy(ECCMath).then( () => { 
-  	deployer.deploy(Secp256k1, ECCMath.address).then(() => {
+deployer.deploy(ECCMath).then( (deployer) => { 
+  	deployer.deploy(Secp256k1, ECCMath.address).then((deployer) => {
           	  deployer.deploy(AnonymousVoting, "0xe621e9f37252493e1eb14ecb0a57711f5e6acc3d", 1, ECCMath.address, Secp256k1.address)})})};
